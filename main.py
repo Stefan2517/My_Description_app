85e8fa38e8f556d5393be485b97f2e3ffe046f8f
Stefan2517 <117020443+Stefan2517@users.noreply.github.com>
2023-11-07 18:27:30 +0200

    Create columns, image, and description

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Stefan Semelbauer")
+    content = """
+    Passionate technology student looking for learning and professional development opportunities. 
+    Follower of constant innovation and exploration in the technological field.
+    """
+    st.info(content)
